d112d4c14616fb03930c1b17cdbefbea567bf0ab
Typescript <poolk008@qq.com>
2022-06-20 23:22:41 +0800

    update

--- WTF/Account/197404344#2/SavedVariables/GTFO.lua
+++ WTF/Account/197404344#2/SavedVariables/GTFO.lua
@@ -1,16 +1,16 @@
 
 GTFOData = {
-	["UnmuteMode"] = false,
-	["Active"] = true,
-	["EnableVibration"] = false,
-	["TrivialDamagePercent"] = 2,
-	["TestMode"] = false,
 	["Sounds"] = {
 		true, -- [1]
 		true, -- [2]
 		true, -- [3]
 		true, -- [4]
 	},
+	["Active"] = true,
+	["EnableVibration"] = false,
+	["TrivialDamagePercent"] = 2,
+	["SoundChannel"] = "Master",
+	["TestMode"] = false,
 	["DataCode"] = "4",
 	["SoundOverrides"] = {
 	},
@@ -18,6 +18,6 @@ GTFOData = {
 	["IgnoreOptions"] = {
 		["Fatigue"] = false,
 	},
-	["SoundChannel"] = "Master",
+	["UnmuteMode"] = false,
 	["TrivialMode"] = false,
 }
